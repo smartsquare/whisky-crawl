4e558e35d5bc0a040a20faf0f37f3605dd51cd5c
Daniel Rosowski <rosowski@smartsquare.de>
2018-03-19 21:40:54 +0100

    Removed spring boot dependencies since we don't need it

---
+++ src/test/kotlin/de/smartsquare/whisky/crawler/BasicParserTest.kt
@@ -0,0 +1,43 @@
+package de.smartsquare.whisky.crawler
+
+import org.assertj.core.api.Assertions.assertThat
+import org.jsoup.Jsoup
+import org.junit.Test
+import java.io.File
+import java.nio.charset.StandardCharsets
+
+class BasicParserTest {
+
+    val parser = BasicParser(Transformer())
+
+    @Test
+    fun should_parse_html_page() {
+        // given
+        val html = File("src/test/resources/html/webpage_all_sorts_p1.html")
+        val document = Jsoup.parse(html, StandardCharsets.UTF_8.displayName())
+
+        // when
+        val whiskyList = parser.readWhiskyListFromHtmlDocument(document)
+
+        // then
+        assertThat(whiskyList).isNotNull
+        assertThat(whiskyList).hasSize(36)
+    }
+
+    @Test
+    fun should_extract_pagination_links() {
+        // given
+        val html = File("src/test/resources/html/webpage_all_sorts_p1.html")
+        val document = Jsoup.parse(html, StandardCharsets.UTF_8.displayName())
+
+        // when
+        val paginationLinks = parser.getPaginationLinks(document)
+
+        // then
+        assertThat(paginationLinks).isNotEmpty
+        assertThat(paginationLinks).hasSize(28)
+        assertThat(paginationLinks).contains(html.absolutePath + "?page=1")
+        assertThat(paginationLinks).contains(html.absolutePath + "?page=28")
+
+    }
+}
