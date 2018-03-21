1cf887b5ecfc5a9a020b1a565e4600d3b90a68e0
Sascha Rodekamp <rodekamp@smartsquare.de>
2018-03-21 18:18:11 +0100

    add DynamoDB data backend

--- src/main/kotlin/de/smartsquare/whisky/CrawlHandler.kt
+++ src/main/kotlin/de/smartsquare/whisky/CrawlHandler.kt
@@ -6,6 +6,7 @@ import de.smartsquare.whisky.crawler.BasicCrawler
 import de.smartsquare.whisky.crawler.BasicParser
 import de.smartsquare.whisky.crawler.Transformer
 import de.smartsquare.whisky.domain.Whisky
+import de.smartsquare.whisky.dynamo.DynamoDbClient
 import org.json.JSONObject
 import java.util.stream.Collectors
 
@@ -13,6 +14,7 @@ class CrawlHandler : RequestHandler<Any, Any> {
 
     override fun handleRequest(input: Any?, ctx: Context?): Any {
         val crawler = BasicCrawler(BasicParser(Transformer()))
+        var dynamo = DynamoDbClient(DynamoDbClient.create())
 
         val baseUrl = "https://www.whiskyworld.de/alle-whiskies"
 
@@ -22,6 +24,8 @@ class CrawlHandler : RequestHandler<Any, Any> {
                 .map { subLink -> crawler.crawl(subLink) }
 
         val whiskyList = allWhiskys.flatten()
+
+        whiskyList.forEach { whisky -> dynamo.write(whisky) }
 
         val headers = hashMapOf("Content-Type" to "application/json")
         return GatewayResponse(JSONObject().put("Output", JSONObject.valueToString(whiskyList)).toString(), headers, 200)
---
+++ src/main/kotlin/de/smartsquare/whisky/dynamo/DynamoDbClient.kt
@@ -0,0 +1,108 @@
+package de.smartsquare.whisky.dynamo
+
+import com.amazonaws.client.builder.AwsClientBuilder
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import com.amazonaws.services.dynamodbv2.document.DynamoDB
+import com.amazonaws.services.dynamodbv2.document.Item
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap
+import com.amazonaws.services.dynamodbv2.model.*
+import de.smartsquare.whisky.domain.Whisky
+import org.slf4j.LoggerFactory
+import java.math.BigDecimal
+import java.time.Instant
+
+
+class DynamoDbClient(val dynamoDB: DynamoDB) {
+
+    companion object Factory {
+        fun create(): DynamoDB = DynamoDB(AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-west-2"))
+                .build())
+
+        val log = LoggerFactory.getLogger(DynamoDbClient::class.java.simpleName)
+        val tableName = "WhiskyPriceEvolution"
+    }
+
+    fun deleteExistingTable() {
+        try {
+            val table = dynamoDB.getTable(tableName)
+
+            table.delete()
+            table.waitForDelete()
+        } catch (e: ResourceNotFoundException) {
+            log.trace("$tableName does not exist")
+        }
+    }
+
+    fun createInitialTable() {
+
+        log.info("Attempting to create table; please wait...")
+
+        val table = dynamoDB.createTable(tableName,
+                listOf(
+                        KeySchemaElement("id_name", KeyType.HASH), // Partition key
+                        KeySchemaElement("id_scrapingDate", KeyType.RANGE) // Sort Key
+                ),
+
+                listOf(
+                        AttributeDefinition("id_name", ScalarAttributeType.S),
+                        AttributeDefinition("id_scrapingDate", ScalarAttributeType.N)
+                ),
+
+                ProvisionedThroughput(10L, 10L))
+
+        table.waitForActive()
+
+        log.info("Success.  Table status: {}", table.getDescription().getTableStatus())
+
+    }
+
+    fun write(whisky: Whisky) {
+
+        val table = dynamoDB.getTable(tableName)
+
+        val name = whisky.name
+        val scrappingMillis = whisky.scrapingDate.toEpochMilli()
+        val description = whisky.description
+
+        val key = "$name $description"
+
+        table.putItem(Item()
+                .withPrimaryKey("id_name", key, "id_scrapingDate", scrappingMillis)
+                .withString("whiskyName", name)
+                .withString("description", description)
+                .withDouble("alcohol", whisky.alcohol)
+                .withDouble("liter", whisky.liter)
+                .withDouble("price", whisky.price.toDouble()))
+
+        log.info("PutItem succeeded: $name ")
+    }
+
+    fun readItem(name: String): List<Whisky> {
+        val table = dynamoDB.getTable(tableName)
+
+        val valueMap: ValueMap = ValueMap()
+                .withString(":name", name)
+        // .withLong(":from", Long.MIN_VALUE)
+        // .withLong(":to", Instant.now().plusSeconds(1000).toEpochMilli())
+
+        val spec = QuerySpec()
+                .withKeyConditionExpression("id_name = :name") //AND id_scrapingDate between :from and :to
+                .withValueMap(valueMap)
+
+        return table.query(spec).map { item ->
+            Whisky(
+                    item.getString("whiskyName"),
+                    item.getString("description"),
+                    item.getDouble("alcohol"),
+                    item.getDouble("liter"),
+                    BigDecimal.valueOf(item.getDouble("price")),
+                    Instant.ofEpochMilli(item.getString("id_scrapingDate").toLong())
+            )
+        }.toList()
+
+    }
+
+
+}
